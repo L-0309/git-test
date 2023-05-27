35554400966244116beeb17366b2bbd11c4b62f5
LiuHaoYu <liu030910@gmail.com>
2023-05-27 16:48:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.atguigu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: git-test
+ * @BelongsPackage: com.atguigu.controller
+ * @Author: Tomcat
+ * @CreateTime: 2023-05-27  16:26
+ * @Description:
+ * @Version: 1.0
+ */
+@RestController
+public class IndexController {
+    @GetMapping("/i")
+    public String u(){
+        return "hi,首次";
+    }
+}
